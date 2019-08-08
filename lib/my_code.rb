94b33ef9506334ad03b1f1ef191c3eaf79ba0c02
Ryan McCahon <mccahonr@yahoo.com>
2019-08-08 14:56:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(source_array, starting_point = 0)
   source_array.length.times do |i|
     starting_point = yield(starting_point, source_array[i])
     if source_array[i] == false
-      return false
+      return true
     end
     end
     starting_point
