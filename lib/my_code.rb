32e7e114d2b5a590dd18e3ff9d266adb446d6268
Ryan McCahon <mccahonr@yahoo.com>
2019-08-08 15:02:56 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new[i] = yield source_array[i]
+    i+=1
+  end
+  return new
+end
+
+def reduce(source_array, starting_point = 0)
+  source_array.length.times do |i|
+    starting_point = yield(starting_point, source_array[i])
+    end
+    if starting_point == 0
+      return true
+    end
+    starting_point
+end
